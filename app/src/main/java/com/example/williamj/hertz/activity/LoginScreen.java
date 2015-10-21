e0847c8733a040d255202f5ec9775d47dd217f9c
William Rivera <willitorivera@gmail.com>
2015-10-21 19:44:23 -0400

    Doing some Material Design with project

--- app/src/main/java/com/example/williamj/hertz/activity/LoginScreen.java
+++ app/src/main/java/com/example/williamj/hertz/activity/LoginScreen.java
@@ -1,4 +1,4 @@
-package com.example.williamj.hertz;
+package com.example.williamj.hertz.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,13 +8,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.williamj.hertz.R;
 
 public class LoginScreen extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
 
     }
---
+++ app/src/main/java/com/example/williamj/hertz/adapter/NavigationDrawerAdapter.java
@@ -0,0 +1,7 @@
+package com.example.williamj.hertz.adapter;
+
+/**
+ * Created by WilliamJ on 10/21/2015.
+ */
+public class NavigationDrawerAdapter {
+}
